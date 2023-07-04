e3c5225bffabad7f8cf6e6b3adc13bd811bbe16f
andrefelype <andrefilype21122014@gmail.com>
2023-07-04 20:12:44 -0300

    add data

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 
 const hourHand = document.querySelector('.hour-hand')
 const minuteHand = document.querySelector('.minute-hand')
-const secondHand = document.querySelector('.second-hand')
+const secondHand = document.querySelector('.second-hand');
+
+const day = document.querySelector('.dia')
 
 
 
@@ -12,13 +14,16 @@ const getTime = () => {
     return {
         hours: date.getHours(),
         minutes: date.getMinutes(),
-        seconds: date.getSeconds()
+        seconds: date.getSeconds(),
+        day: date.getDay(),
+        month: date.getMonth(),
+        year: date.getFullYear(),
     };
 }
 
 setInterval( () => {
 
-    const {seconds, minutes, hours} = getTime()
+    const {seconds, minutes, hours,} = getTime()
 
     secondHand.style.transform = `translate(0, -50%) rotate(${seconds * 6}deg)`
 
@@ -26,6 +31,12 @@ setInterval( () => {
 
     hourHand.style.transform = `translate(0, -50%) rotate(${hours * 30}deg)`
 
-
-
 } , 1000)
+
+const dataAtualizada = () => {
+
+    const{day, month, year} = getTime()
+
+}
+
+dataAtualizada()
